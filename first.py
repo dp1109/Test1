e1ff09b0dc3333dddff650d2c0b33de6209c6316
dp1109 <143241884+dp1109@users.noreply.github.com>
2023-09-16 11:33:58 +0530

    Update first.py

--- first.py
+++ first.py
@@ -1,2 +1,1 @@
-Python 3.11.0rc2 (main, Sep 11 2022, 20:22:52) [MSC v.1933 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
+print("hrllo Harshal sir")
